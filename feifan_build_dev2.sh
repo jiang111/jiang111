7287845b116100055b5e2a6ed9277bacaf8c8cb6
NewTab <425698907@qq.com>
2023-11-01 23:15:39 +0800

    Update feifan_build_dev2.sh

--- feifan_build_dev2.sh
+++ feifan_build_dev2.sh
@@ -12,8 +12,3 @@ echo "开始构建2套环境的 web:"
 flutter build web --release --web-renderer canvaskit --target ./lib/main_dev2.dart
 echo "构建完成"
 dart ./bin/patch.dart http://mapptest2.feifan.art
-cd build
-echo "开始打包 web.tar.gz"
-tar -zcf web.tar.gz ./web/
-echo 'web.tar.gz 打包完成,文件路径:'
-echo $(pwd)/web.tar.gz
