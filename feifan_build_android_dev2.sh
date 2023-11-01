1018b05a0bef733838583937e389a325c7644eec
NewTab <425698907@qq.com>
2023-11-01 17:40:52 +0800

    Add files via upload

---
+++ feifan_build_android_dev2.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/flutter/flutter.git -b stable
+cd ./flutter/bin
+chmod 774 flutter
+chmod 774 dart
+./flutter doctor
+export PATH=PATH=$PATH:$(pwd)
+cd ..
+echo flutter.sdk=$(pwd) > emas_config.local.properties
+cat emas_config.local.properties > ../android/local.properties
+cd ..
+echo "开始构建2套环境的 Android 包:"
+flutter build apk --release --target ./lib/main_dev2.dart
+echo "构建完成"
+echo 'Android 包文件路径:'
+echo $(pwd)/build/app/outputs/flutter-apk/app-release.apk
