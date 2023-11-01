450051321876789d7a72b6a18ae235983a357c24
NewTab <425698907@qq.com>
2023-11-01 17:00:47 +0800

    Add files via upload

--- feifan_build_dev2.sh
+++ feifan_build_dev2.sh
@@ -8,10 +8,10 @@ cd ..
 echo flutter.sdk=$(pwd) > emas_config.local.properties
 cat emas_config.local.properties > ../android/local.properties
 cd ..
-echo "开始构建1套环境的 web:"
-flutter build web --release --target ./lib/main_dev.dart
+echo "开始构建2套环境的 web:"
+flutter build web --release --target ./lib/main_dev2.dart
 echo "构建完成"
-dart ./bin/patch.dart http://mapptest.feifan.art
+dart ./bin/patch.dart http://mapptest2.feifan.art
 cd build
 echo "开始打包 web.tar.gz"
 tar -zcf web.tar.gz ./web/
