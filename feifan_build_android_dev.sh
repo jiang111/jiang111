d65b01c839ca3080db73d4f9b59a3b04c4b976ee
NewTab <425698907@qq.com>
2023-11-06 15:33:23 +0800

    Add files via upload

--- feifan_build_android_dev.sh
+++ feifan_build_android_dev.sh
@@ -1,36 +1,30 @@
+echo "====================================================================="
+echo "Start to install jdk 11"
+echo "====================================================================="
+ 
 
-while true; do
-    echo "y" | apt-get install software-properties-common
-    if [ $? -eq 0 ]; then
-        echo "命令执行成功。"
-        break
-    else
-        echo "命令执行失败。继续尝试。"
-    fi
-done
+mkdir java11
+cd java11
+wget https://raw.githubusercontent.com/jiang111/jiang111/master/jdk-11/Archive.zip
+wget https://raw.githubusercontent.com/jiang111/jiang111/master/jdk-11/Archive2.zip
+wget https://raw.githubusercontent.com/jiang111/jiang111/master/jdk-11/Archive3.zip
 
-add-apt-repository ppa:openjdk-r/ppa
+tar -zxf Archive.zip
+tar -zxf Archive2.zip
+tar -zxf Archive3.zip
 
-while true; do
-    echo "y" | apt-get update
-    if [ $? -eq 0 ]; then
-        echo "命令执行成功。"
-        break
-    else
-        echo "命令执行失败。继续尝试。"
-    fi
-done
-while true; do
-    echo "y" | apt install -y openjdk-11-jdk
-    if [ $? -eq 0 ]; then
-        echo "命令执行成功。"
-        break
-    else
-        echo "命令执行失败。继续尝试。"
-    fi
-done
+cd ..
+
+export JAVA_HOME=/root/workspace/NewFeiFanApp_7iSy/java11
+export JRE_HOME=${JAVA_HOME}/jre
+export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
+export PATH=${JAVA_HOME}/bin:$PATH
 java -version
 
+echo "====================================================================="
+echo "Start to install android sdk"
+echo "====================================================================="
+ 
 wget https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
 
 tar -zxf  android-sdk_r24.4.1-linux.tgz
@@ -50,18 +44,40 @@ source /etc/profile
 export PATH="/root/workspace/NewFeiFanApp_7iSy/android-sdk-linux/tools:$PATH"
 export PATH="/root/workspace/NewFeiFanApp_7iSy/android-sdk-linux/platform-tools:$PATH"
 
-while true; do
-    echo "y" | android update sdk -u -s -a -t platform-tools,android-33,cmdline-tools
-    if [ $? -eq 0 ]; then
-        echo "命令执行成功。"
-        break
-    else
-        echo "命令执行失败。继续尝试。"
-    fi
+
+function install_sdk {
+  android update sdk -u -s -a -t "$1"
+}
+
+function fetch_non_obsoled_package_indices {
+  # Fetch the sdk list using non-https connections
+  android list sdk -u -s -a |\
+    # Filter obsoleted packages
+    sed '/\(Obsolete\)/d' |\
+    # Filter to take only the index number of package
+    sed 's/^[ ]*\([0-9]*\).*/\1/' |\
+    # Remove the empty lines
+    sed -n 's/^[^ $]/\0/p'
+}
+
+for package_index in  $(fetch_non_obsoled_package_indices)
+do
+  echo "====================================================================="
+  echo "Start to install package:  ${package_index}"
+  echo "====================================================================="
+  # Auto accept license
+  echo -e "y" | install_sdk "${package_index}"
+  echo
+  echo
 done
+
+
 export ANDROID_SDK_ROOT="/root/workspace/NewFeiFanApp_7iSy/android-sdk-linux"
 
-
+echo "====================================================================="
+echo "Start to install flutter sdk"
+echo "====================================================================="
+ 
 git clone https://github.com/flutter/flutter.git -b stable
 cd ./flutter/bin
 chmod 774 flutter
@@ -70,10 +86,13 @@ chmod 774 dart
 export PATH=PATH=$PATH:$(pwd)
 cd ..
 echo flutter.sdk=$(pwd) > emas_config.local.properties
-echo flutter.sdk="/root/workspace/NewFeiFanApp_7iSy/android-sdk-linux" > emas_config.local.properties
+echo sdk.dir="/root/workspace/NewFeiFanApp_7iSy/android-sdk-linux" > emas_config.local.properties
 cat emas_config.local.properties > ../android/local.properties
 cd ..
-echo "开始构建1套环境的 Android 包:"
+echo "====================================================================="
+echo "Start to 构建一套环境:"
+echo "====================================================================="
+ 
 flutter build apk --release --target ./lib/main_dev.dart
 echo "构建完成"
 echo 'Android 包文件路径:'
