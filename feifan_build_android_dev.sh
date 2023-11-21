8d19756d93e88a1e5a2a21b16792407c20c63fa9
NewTab <425698907@qq.com>
2023-11-21 13:29:24 +0800

    Update feifan_build_android_dev.sh

--- feifan_build_android_dev.sh
+++ feifan_build_android_dev.sh
@@ -7,7 +7,7 @@ echo "====================================================================="
 # 更新java 版本,老版本无法编译
 wget -q https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
 tar -xvf jdk-17_linux-x64_bin.tar.gz
-export JAVA_HOME=/root/workspace/NewFeiFanApp_android_dev/jdk-17.0.9
+export JAVA_HOME=`pwd`/jdk-17.0.9
 export JRE_HOME=${JAVA_HOME}/jre
 export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
 export PATH=${JAVA_HOME}/bin:$PATH
@@ -28,7 +28,7 @@ mkdir sdk
 mkdir sdk/cmdline-tools
 mv latest sdk/cmdline-tools
 
-export ANDROID_HOME=/root/workspace/NewFeiFanApp_android_dev/sdk
+export ANDROID_HOME=`pwd`/sdk
 
 export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
 
@@ -42,12 +42,14 @@ export PATH="$ANDROID_HOME/tools:$PATH"
 export PATH="$ANDROID_HOME/platform-tools:$PATH"
 export ANDROID_SDK_ROOT="$ANDROID_HOME"
 
-# 重新配jdk 保证环境是新版的 java
-export JAVA_HOME=/root/workspace/NewFeiFanApp_android_dev/jdk-17.0.9
+
+# 重新配jdk 保证环境是新版的
+export JAVA_HOME=`pwd`/jdk-17.0.9
 export JRE_HOME=${JAVA_HOME}/jre
 export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
 export PATH=${JAVA_HOME}/bin:$PATH
 java --version
+
 
 echo "====================================================================="
 echo "Start to install flutter sdk"
@@ -55,10 +57,11 @@ echo "====================================================================="
  
 git clone https://github.com/flutter/flutter.git -b 3.13.9
 export PATH="$PATH:`pwd`/flutter/bin"
-echo flutter.sdk="/root/workspace/NewFeiFanApp_android_dev/flutter" > emas_config.local.properties
-echo sdk.dir="/root/workspace/NewFeiFanApp_android_dev/sdk" > emas_config.local.properties
+echo flutter.sdk="$(pwd)/flutter" > emas_config.local.properties
+echo sdk.dir="$(pwd)/sdk" > emas_config.local.properties
 cat emas_config.local.properties > ./android/local.properties
 
+flutter config --android-sdk `pwd`/sdk
 
 echo "y" | flutter doctor
 
