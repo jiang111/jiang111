684e9951dffd86e82f1b86320c2030ad53a67e49
NewTab <425698907@qq.com>
2023-11-21 09:58:29 +0800

    Update feifan_build_android_dev2.sh

--- feifan_build_android_dev2.sh
+++ feifan_build_android_dev2.sh
@@ -1,55 +1,36 @@
 
+
 echo "====================================================================="
-echo "Start to install android sdk"
+echo "Start to install java 17 sdk"
 echo "====================================================================="
  
-echo "y" |  apt update
-
-echo "y" | apt install android-sdk
-
-
-export ANDROID_HOME="/usr/lib/android-sdk"
-if ! grep "ANDROID_HOME=/usr/lib/android-sdk" /etc/profile 
-then
-echo "ANDROID_HOME=/usr/lib/android-sdk" | sudo tee -a /etc/profile
-echo "export ANDROID_HOME" | sudo tee -a /etc/profile
-
-echo "PATH=${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:$PATH" | sudo tee -a /etc/profile
-echo "export PATH" | sudo tee -a /etc/profile
-fi
-
-source /etc/profile  
-export PATH="/usr/lib/android-sdk/tools:$PATH"
-export PATH="/usr/lib/android-sdk/platform-tools:$PATH"
-
-# 下载sdk 相关的配置
-
-
-
-cd /usr/lib/android-sdk
-
+# 更新java 版本,老版本无法编译
+wget -q https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
+tar -xvf jdk-17_linux-x64_bin.tar.gz
+export JAVA_HOME=/root/workspace/NewFeiFanApp_android_dev2/jdk-17.0.9
+export JRE_HOME=${JAVA_HOME}/jre
+export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
+export PATH=${JAVA_HOME}/bin:$PATH
+java --version
 
 
 echo "====================================================================="
 echo "Start to download Android commandlinetools"
 echo "====================================================================="
- 
 
-wget -q https://dl.google.com/android/repository/commandlinetools-linux-6609375_latest.zip
 
+wget -q https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip
 
 unzip commandlinetools-linux-6609375_latest.zip -d cmdline-tools
 
+mv cmdline-tools latest
+mkdir sdk
+mkdir sdk/cmdline-tools
+mv latest sdk/cmdline-tools
+
+export ANDROID_HOME=/root/workspace/NewFeiFanApp_android_dev2/sdk
+
 export PATH=$ANDROID_HOME/cmdline-tools/tools/bin:$PATH
-
-
-echo "====================================================================="
-echo "Start to use sdkmanager"
-echo "====================================================================="
-
-echo "y" | sdkmanager  --licenses
-
-echo "y" | sdkmanager --update
 
 echo "y" | sdkmanager "platform-tools" "system-images;android-34;default;arm64-v8a" "build-tools;34.0.0"
 
@@ -57,29 +38,9 @@ echo "y" | sdkmanager --install "cmdline-tools;latest"
 
 echo "y" | sdkmanager  --licenses
 
-
-cd /root/workspace/NewFeiFanApp_android_dev2
-
-
-echo "====================================================================="
-echo "Start to install java 17 sdk"
-echo "====================================================================="
- 
-# 更新java 版本,老版本无法编译
-# wget -q https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
-
-
-# tar -xvf jdk-17_linux-x64_bin.tar.gz
-
-# export JAVA_HOME=/root/workspace/NewFeiFanApp_android_dev2/jdk-17.0.9
-# export JRE_HOME=${JAVA_HOME}/jre
-# export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
-# export PATH=${JAVA_HOME}/bin:$PATH
-
-
-java --version
-
-export ANDROID_SDK_ROOT="/usr/lib/android-sdk"
+export PATH="$ANDROID_HOME/tools:$PATH"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
+export ANDROID_SDK_ROOT="$ANDROID_HOME"
 
 
 echo "====================================================================="
